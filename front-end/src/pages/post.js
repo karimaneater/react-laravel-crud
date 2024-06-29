67cb81e0cfc5b614ad5e616436d6ca59570556de
Rollen Ricafort <53422684+karimaneater@users.noreply.github.com>
2024-06-29 20:33:54 +0800

    Revert "Add post API"

---
+++ front-end/src/pages/post.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+
+const Post = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost/react-laravel-crud/back-end/public/api/home")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  console.log(posts, "posts");
+  const columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "title", headerName: "Title", width: 130 },
+    {
+      field: "description",
+      headerName: "Description",
+      description: "This column has a value getter and is not sortable.",
+      width: 160,
+    },
+    { field: "created_at", headerName: "Created At", width: 130 },
+    {
+      field: "updated_at",
+      headerName: "Updated At",
+      type: "number",
+      width: 90,
+    },
+   
+  ];
+
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={posts}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+    </div>
+  );
+};
+
+export default Post;

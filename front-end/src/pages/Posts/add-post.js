ced5010f9ab805c9722b2d6b710e16ee127f9c17
Rollen Ricafort <rollenricafort011@gmail.com>
2024-06-29 19:55:59 +0800

    Add Posts API

--- front-end/src/pages/Posts/add-post.js
+++ front-end/src/pages/Posts/add-post.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '@mui/material/Button';
 
 const AddPost = () => {
@@ -6,9 +6,34 @@ const AddPost = () => {
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
 
-    const handleSubmit = () => {
-    // add post here
-    }
+      const addPosts = async (title, description) => {
+        await fetch('http://localhost/react-laravel-crud/back-end/public/api/home', {
+        method: 'POST',
+        body: JSON.stringify({
+           title: title,
+           description: description,
+        }),
+        headers: {
+           'Content-type': 'application/json; charset=UTF-8',
+        },
+        })
+        .then((response) => response.json())
+        .then((data) => {
+         console.log(data);
+          //  setPosts((posts) => [data, ...posts]);
+           setTitle('');
+           setDescription('');
+        })
+        .catch((err) => {
+           console.log(err.message);
+        });
+        }
+
+        const handleSubmit = (e) => {
+          e.preventDefault();
+          addPosts(title, description);
+       };    
+
 
   return (
     <div>
